MITRECND/WhoDat

commit 2475dd5ddcc9dc3404560d9c9763fb7e121028b2

Fix problems with list option.
Don't pass a dictionary into the search handler. Just pass None and let
the appropriate handler translate that into what it filters by default.

Fix various problems with list option and historical data. If you filtered
for domainName there would be no details key.

low_version and high_version should be None, not 'null'.

pydat/pydat/handlers/mongo.py
@@ -147,7 +147,7 @@ def dataTableSearch(key, value, skip, pagesize, sortset, sfilter, low, high):
     results['success'] = True
     return results
 
-def search(key, value, filt={}, limit=settings.LIMIT, low = None, high = None, versionSort = False):
+def search(key, value, filt=None, limit=settings.LIMIT, low = None, high = None, versionSort = False):
     results = {'success': False}
     try:
         coll = mongo_connector(settings.COLL_WHOIS)
@@ -160,6 +160,15 @@ def search(key, value, filt={}, limit=settings.LIMIT, low = None, high = None, v
     else:
         search_document = {key: value}
 
+    # Always filter out _id.
+    filt_document = {'_id': False}
+
+    # If filter key requested, use it.
+    if filt == 'domainName':
+        filt_document[filt] = 1
+    elif filt != None:
+        filt_document['details.' + filt] = 1
+
     if low != None:
         if low == high or high is None:
             search_document['dataVersion'] = int(low)
@@ -169,17 +178,18 @@ def search(key, value, filt={}, limit=settings.LIMIT, low = None, high = None, v
     sortset = None
     if versionSort:
         sortset = [('dataVersion', pymongo.ASCENDING)] 
-    domains = coll.find(search_document, filt, limit=limit, sort = sortset)
+    domains = coll.find(search_document, filt_document, limit=limit, sort = sortset)
 
     results['total'] = domains.count()
     results['data'] = []
     for domain in domains:
-        dom = domain['details']
-        dom['domainName'] = domain['domainName']
-        dom['Version'] = domain['dataVersion']
-        results['data'].append(dom)
+        # Take each key in details (if any) and stuff it in top level dict.
+        if 'details' in domain:
+            for k, v in domain['details'].iteritems():
+                domain[k] = v
+            del domain['details']
+        results['data'].append(domain)
 
-    #results['data'] = [domain for domain in domains]
     results['avail'] = len(results['data'])
     results['success'] = True
     return results

pydat/pydat/ajax.py
@@ -90,7 +90,7 @@ def domains(request, key, value, low = None, high = None):
     if key == 'domainName':
         versionSort = True
 
-    results = handler.search(key, value, filt = {'_id': False}, low = low, high = high, versionSort = versionSort)
+    results = handler.search(key, value, filt = None, low = low, high = high, versionSort = versionSort)
     if results['success'] == False:
         return __renderErrorJSON__(results['message'])
 
@@ -105,7 +105,7 @@ def domain(request, domainName = None, low = None, high = None):
             return __renderErrorJSON__('Requires Domain Name Argument')
         domainName = urllib.unquote(domainName)
 
-        results = handler.search('domainName', domainName, filt={'_id': False}, low = low, high = high, versionSort = True)
+        results = handler.search('domainName', domainName, filt=None, low = low, high = high, versionSort = True)
 
         return HttpResponse(json.dumps(results), content_type='application/json')
         if results['success']: #Clean up the data
@@ -124,8 +124,8 @@ def domain_diff(request, domainName = None, v1 = None, v2 = None):
             return __renderErrorJSON__('Required Parameters Missing')
         domainName = urllib.unquote(domainName)
 
-        v1_res = handler.search('domainName', domainName, filt={'_id':False}, low = int(v1))
-        v2_res = handler.search('domainName', domainName, filt={'_id':False}, low = int(v2))
+        v1_res = handler.search('domainName', domainName, filt=None, low = int(v1))
+        v2_res = handler.search('domainName', domainName, filt=None, low = int(v2))
 
         try:
             v1_res = v1_res['data'][0]

pydat/pydat/views.py
@@ -78,15 +78,12 @@ def domains(request, key=None, value=None):
         low_version = handler.lastVersion()
         high_version = low_version
     else:
-        low_version = 'null' 
-        high_version = 'null'
+        low_version = None
+        high_version = None
     
 
     if fmt == 'list': #Only filter if a list was requested
-        filt = {filt_key: 1, '_id': False}
-    else:
-        filt = {'_id': False}
-
+        filt = filt_key
 
     #All web searches are AJAXy
     if fmt == "normal":

commit fc2b9932649c7cf69e20a63ebd31b0c2db755c7e

Fix missing version.
This was causing historical records and diffing to be broken.

This was broken in commit 2475dd5ddcc9dc3404560d9c9763fb7e121028b2.

pydat/pydat/handlers/mongo.py
@@ -188,6 +188,8 @@ def search(key, value, filt=None, limit=settings.LIMIT, low = None, high = None,
             for k, v in domain['details'].iteritems():
                 domain[k] = v
             del domain['details']
+        domain['Version'] = domain['dataVersion']
+        del domain['dataVersion']
         results['data'].append(domain)
 
     results['avail'] = len(results['data'])