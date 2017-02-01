e8991c8f8861a89ccc8f3a94bbf4fc70d2b0b9cb
Murad Khan <mkhan@mitre.org>
2017-02-01 15:22:46 -0500

    Update code to run properly on newer version of django

--- pydat/pydat/urls.py
+++ pydat/pydat/urls.py
@@ -1,50 +1,51 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.conf import settings
+from pydat import ajax, views
 
-urlpatterns = patterns('',
-  url(r'^$', 'pydat.views.index', name='index'),
+urlpatterns = [
+  url(r'^$', views.index, name='index'),
 
-  url(r'^domains/(?P<key>.*)/(?P<value>.*)/$', 'pydat.views.domains'),
-  url(r'^domains/$', 'pydat.views.domains', name='domains'),
+  url(r'^domains/(?P<key>.*)/(?P<value>.*)/$', views.domains),
+  url(r'^domains/$', views.domains, name='domains'),
 
-  url(r'^pdns_results/(?P<domain>.*)/$', 'pydat.views.pdns', name='pdns_rest'),
-  url(r'^pdns_results/$', 'pydat.views.pdns', name='pdns_results'),
-  url(r'^pdns_search/$', 'pydat.views.pdns_index', name='pdns'),
+  url(r'^pdns_results/(?P<domain>.*)/$', views.pdns, name='pdns_rest'),
+  url(r'^pdns_results/$', views.pdns, name='pdns_results'),
+  url(r'^pdns_search/$', views.pdns_index, name='pdns'),
 
-  url(r'^pdnsr_results/(?P<key>.*)/(?P<value>.*)/$', 'pydat.views.pdns_r', name='pdns_r_rest'),
-  url(r'^pdnsr_results/$', 'pydat.views.pdns_r', name='pdns_r_results'),
-  url(r'^pdnsr_search/$', 'pydat.views.rpdns_index', name='pdns_r'),
+  url(r'^pdnsr_results/(?P<key>.*)/(?P<value>.*)/$', views.pdns_r, name='pdns_r_rest'),
+  url(r'^pdnsr_results/$', views.pdns_r, name='pdns_r_results'),
+  url(r'^pdnsr_search/$', views.rpdns_index, name='pdns_r'),
 
-  url(r'^ajax/metadata/$', 'pydat.ajax.metadata'),
-  url(r'^ajax/metadata/(?P<version>.*)/$', 'pydat.ajax.metadata'),
+  url(r'^ajax/metadata/$', ajax.metadata),
+  url(r'^ajax/metadata/(?P<version>.*)/$', ajax.metadata),
 
-  url(r'^ajax/domain/(?P<domainName>.*)/diff/(?P<v1>.*)/(?P<v2>.*)/$', 'pydat.ajax.domain_diff'),
-  url(r'^ajax/domain/(?P<domainName>.*)/(?P<low>.*)/(?P<high>.*)/$', 'pydat.ajax.domain'),
-  url(r'^ajax/domain/(?P<domainName>.*)/latest/$', 'pydat.ajax.domain_latest'),
-  url(r'^ajax/domain/(?P<domainName>.*)/(?P<low>.*)/$', 'pydat.ajax.domain'),
-  url(r'^ajax/domain/(?P<domainName>.*)/$', 'pydat.ajax.domain'),
-  url(r'^ajax/domain/$', 'pydat.ajax.domain', name='ajax_domain'),
+  url(r'^ajax/domain/(?P<domainName>.*)/diff/(?P<v1>.*)/(?P<v2>.*)/$', ajax.domain_diff),
+  url(r'^ajax/domain/(?P<domainName>.*)/(?P<low>.*)/(?P<high>.*)/$', ajax.domain),
+  url(r'^ajax/domain/(?P<domainName>.*)/latest/$', ajax.domain_latest),
+  url(r'^ajax/domain/(?P<domainName>.*)/(?P<low>.*)/$', ajax.domain),
+  url(r'^ajax/domain/(?P<domainName>.*)/$', ajax.domain),
+  url(r'^ajax/domain/$', ajax.domain, name='ajax_domain'),
 
-  url(r'^ajax/domains/(?P<key>.*)/(?P<value>.*)/(?P<low>.*)/(?P<high>.*)/$', 'pydat.ajax.domains' ),
-  url(r'^ajax/domains/(?P<key>.*)/(?P<value>.*)/latest/$', 'pydat.ajax.domains_latest' ),
-  url(r'^ajax/domains/(?P<key>.*)/(?P<value>.*)/(?P<low>.*)/$', 'pydat.ajax.domains' ),
-  url(r'^ajax/domains/(?P<key>.*)/(?P<value>.*)/$', 'pydat.ajax.domains' ),
-  url(r'^ajax/domains/$', 'pydat.ajax.domains', name='ajax_domains'),
+  url(r'^ajax/domains/(?P<key>.*)/(?P<value>.*)/(?P<low>.*)/(?P<high>.*)/$', ajax.domains ),
+  url(r'^ajax/domains/(?P<key>.*)/(?P<value>.*)/latest/$', ajax.domains_latest ),
+  url(r'^ajax/domains/(?P<key>.*)/(?P<value>.*)/(?P<low>.*)/$', ajax.domains ),
+  url(r'^ajax/domains/(?P<key>.*)/(?P<value>.*)/$', ajax.domains ),
+  url(r'^ajax/domains/$', ajax.domains, name='ajax_domains'),
 
-  url(r'^ajax/dataTable/(?P<key>.*)/(?P<value>.*)/(?P<low>.*)/(?P<high>.*)/$', 'pydat.ajax.dataTable' ),
-  url(r'^ajax/dataTable/$', 'pydat.ajax.dataTable', name='ajax_dataTable'),
+  url(r'^ajax/dataTable/(?P<key>.*)/(?P<value>.*)/(?P<low>.*)/(?P<high>.*)/$', ajax.dataTable ),
+  url(r'^ajax/dataTable/$', ajax.dataTable, name='ajax_dataTable'),
 
 
-  url(r'^ajax/resolve/(?P<domainName>.*)/$', 'pydat.ajax.resolve'),
-  url(r'^ajax/resolve/$', 'pydat.ajax.resolve', name='ajax_resolve'),
-)
+  url(r'^ajax/resolve/(?P<domainName>.*)/$', ajax.resolve),
+  url(r'^ajax/resolve/$', ajax.resolve, name='ajax_resolve'),
+]
 
 if settings.HANDLER == 'es': #Enable ES specific urls
-    urlpatterns += patterns('', 
-      url(r'^advdomains/$', 'pydat.views.advdomains', name='advdomains'),
-      url(r'^ajax/query/$', 'pydat.ajax.advanced_search', name='ajax_advanced'),
-      url(r'^ajax/advDataTable/$', 'pydat.ajax.advDataTable', name='ajax_advDataTable'),
-      url(r'^stats/$', 'pydat.views.stats', name='stats'),
-      url(r'^help/$', 'pydat.views.help', name='help'),
-    )
+    urlpatterns.extend([
+      url(r'^advdomains/$', views.advdomains, name='advdomains'),
+      url(r'^ajax/query/$', ajax.advanced_search, name='ajax_advanced'),
+      url(r'^ajax/advDataTable/$', ajax.advDataTable, name='ajax_advDataTable'),
+      url(r'^stats/$', views.stats, name='stats'),
+      url(r'^help/$', views.help, name='help'),
+    ])
 
