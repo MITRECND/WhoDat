f13766c3d87168357ff222ddc32bd908aa22996f
Murad Khan <mkhan@mitre.org>
2017-06-14 15:36:33 -0400

    Update settings related to handling of static files

--- pydat/pydat/settings.py
+++ pydat/pydat/settings.py
@@ -137,7 +137,6 @@ STATICFILES_DIRS = [
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(SITE_ROOT, 'static'),
 ]
 
 # List of finder classes that know how to find static files in
@@ -145,7 +144,6 @@ STATICFILES_DIRS = [
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 ]
 
 # Make this unique, and don't share it with anybody.
