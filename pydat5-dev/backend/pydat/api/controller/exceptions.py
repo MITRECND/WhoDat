e948c2da58d337f8c002e92fc0fe1b40e2acb96e
Hanna Xu <hxu@mitre.org>
2020-07-09 10:43:51 -0700

    update key to error to differentiate from 200 message

--- pydat5-dev/backend/pydat/api/controller/exceptions.py
+++ pydat5-dev/backend/pydat/api/controller/exceptions.py
@@ -13,7 +13,7 @@ class InvalidUsage(Exception):
 
     def to_dict(self):
         rv = dict(self.payload or ())
-        rv["message"] = self.message
+        rv["error"] = self.message
         return rv
 
 
