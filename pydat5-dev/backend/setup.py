70c161b1f4d694de0fe3f8389881fdee9cd6d85f
Hanna Xu <hxu@mitre.org>
2020-07-13 10:59:09 -0700

    update version

--- pydat5-dev/backend/setup.py
+++ pydat5-dev/backend/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_namespace_packages, setup
 
 setup(
     name="pydat",
-    version="1.0.0",
+    version="5.0.0",
     packages=find_namespace_packages(include=['pydat.*']),
     include_package_data=True,
     zip_safe=False,
