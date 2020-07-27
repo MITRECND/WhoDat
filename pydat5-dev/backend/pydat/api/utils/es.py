a553ad3ea0984a5d7e8d61be12bff5fd53102430
Hanna Xu <hcxu@princeton.edu>
2020-07-27 15:39:04 -0700

    move config access to search

--- pydat5-dev/backend/pydat/api/utils/es.py
+++ pydat5-dev/backend/pydat/api/utils/es.py
@@ -7,6 +7,14 @@ class ElasticsearchError(Exception):
     pass
 
 
+class ConnectionError(ElasticsearchError):
+    pass
+
+
+class NotFoundError(ElasticsearchError):
+    pass
+
+
 def lastVersion():
     pass
 
@@ -19,9 +27,10 @@ def createAdvancedQuery(query, skip, size, unique, sort=None):
     pass
 
 
-def search(key, value, limit, filt=None, low=None, high=None, versionSort=False):
-    return {'success': True, 'aaData': [], 'iTotalRecords': 0, 'iTotalDisplayRecords': 0}
+def search(key, value, filt=None, low=None, high=None, versionSort=False):
+    limit = current_app.config["LIMIT"]
+    return {'success': True, 'data': [], 'total': 100, 'avail': 0}
 
 
 def advanced_search(query, skip=0, size=20, unique=False):
-    return {'success': True, 'total': 0, 'data': []}
+    return {'success': True, 'total': 100, 'data': []}
