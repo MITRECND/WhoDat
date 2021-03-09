7a4353e51de395cf2d04b11c5f2f45acb540849a
Murad Khan <mkhan@mitre.org>
2021-03-09 16:13:56 -0500

    Modify dropdown cell to only use icon to make it friendlier on chrome

--- pydat5-dev/frontend/src/components/helpers/dropdown_cell.jsx
+++ pydat5-dev/frontend/src/components/helpers/dropdown_cell.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import clsx from 'clsx'
 
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import IconButton from '@material-ui/core/IconButton'
@@ -7,14 +8,25 @@ import { makeStyles } from '@material-ui/core';
 
 
 const useStyles = makeStyles((theme) => ({
-    unSelectable: {
-        userSelect: 'none'
+    linkCell: {
+        cursor: 'pointer'
+    },
+    buttonRoot: {
+        display: 'inline-flex',
+        alignItems: 'flex-start',
+        flexWrap: 'wrap',
+    },
+    buttonLabel: {
+        width: '100%',
+        display: 'inline-flex',
     }
 }))
 
 export const DropDownCell = (props) => {
     const classes = useStyles()
     const [anchorEl, setAnchorEl] = useState(null)
+    // TODO cleanup implemention and pick one
+    const useDropDownIcon = false
 
     const handleClick = (e) => {
         setAnchorEl(e.currentTarget)
@@ -26,33 +38,63 @@ export const DropDownCell = (props) => {
 
     const copyFriendly = props.copyFriendly || false
 
-    return (
-        <React.Fragment>
-            {copyFriendly === false &&
-                <React.Fragment>
-                    <IconButton
-                        aria-controls={`${props.friendly}-menu`}
-                        onClick={handleClick}
-                        size='small'
-                    >
-                        <ArrowDropDownIcon />
-                    </IconButton>
-                    {anchorEl !== null &&
-                        <Menu
-                            anchorEl={anchorEl}
-                            keepMounted
-                            open={Boolean(anchorEl)}
-                            onClose={handleClose}
+    if (useDropDownIcon) {
+        return (
+            <React.Fragment>
+                {copyFriendly === false &&
+                    <React.Fragment>
+                        <IconButton
+                            classes={{
+                                root: classes.buttonRoot,
+                                label: classes.buttonLabel
+                            }}
+                            aria-controls={`${props.friendly}-menu`}
+                            onClick={handleClick}
+                            size='small'
                         >
-                            {props.children}
-                        </Menu>
-                    }
-                </React.Fragment>
-            }
-            {props.value}
-        </React.Fragment>
+                            <ArrowDropDownIcon />
+                        </IconButton>
+                        {anchorEl !== null &&
+                            <Menu
+                                anchorEl={anchorEl}
+                                keepMounted
+                                open={Boolean(anchorEl)}
+                                onClose={handleClose}
+                            >
+                                {props.children}
+                            </Menu>
+                        }
+                    </React.Fragment>
+                }
+                {props.value}
+            </React.Fragment>
 
-    )
+        )
+    } else {
+        return (
+            <React.Fragment>
+                <span
+                    onClick={handleClick}
+                    className={clsx(classes.linkCell, classes.buttonRoot)}
+                >
+                    {copyFriendly === false &&
+                        <ArrowDropDownIcon/>
+                    }
+                    {props.value}
+                </span>
+                {anchorEl !== null &&
+                    <Menu
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        {props.children}
+                    </Menu>
+                }
+            </React.Fragment>
+        )
+    }
 }
 
 export default DropDownCell
